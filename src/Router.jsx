82141f9d5b9acd77a93652394eee3b652bf44002
ascean <106957442+ascean@users.noreply.github.com>
2022-11-14 09:35:43 +0100

    fix bug router

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/header/Header";
-import Error from "./pages/error/Error";
-import User from "./pages/user/User";
+import Error from "./pages/Error/Error";
+import User from "./pages/User/User";
 
 const Router = () => {
     return (
